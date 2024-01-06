80cf0a1053b15df01d3bdbbe4ad766c8f266b121
dingmandeng <makemake1293@gmail.com>
2024-01-05 19:42:43 -0800

    create admin app and update pom

--- admin/src/main/java/org/myShortLink/admin/Main.java
+++
@@ -1,7 +0,0 @@
-package org.myShortLink.admin;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-}
---
+++ admin/src/main/java/org/myShortLink/admin/MyShortLinkAdminApplication.java
@@ -0,0 +1,12 @@
+package org.myShortLink.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyShortLinkAdminApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MyShortLinkAdminApplication.class, args);
+    }
+
+}
