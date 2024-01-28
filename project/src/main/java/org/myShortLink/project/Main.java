0bd20cbed1d39082c70856cb9587c76f37a10916
dingmandeng <makemake1293@gmail.com>
2024-01-27 23:52:53 -0800

    impl basic link infrastructure II

--- project/src/main/java/org/myShortLink/project/Main.java
+++
@@ -1,7 +0,0 @@
-package org.myShortLink.project;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-}
---
+++ project/src/main/java/org/myShortLink/project/MyShortLinkProjectApplication.java
@@ -0,0 +1,11 @@
+package org.myShortLink.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyShortLinkProjectApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MyShortLinkProjectApplication.class, args);
+    }
+}
