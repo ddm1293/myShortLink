b47cdd05b2aa78390cba4e18363693a0d7576d85
dingmandeng <makemake1293@gmail.com>
2024-01-30 13:24:31 -0800

    update ShortLinkPageRespDTO

--- project/src/main/java/org/myShortLink/project/dto/resp/ShortLinkPageRespDTO.java
+++ project/src/main/java/org/myShortLink/project/dto/resp/ShortLinkPageRespDTO.java
@@ -3,7 +3,7 @@ package org.myShortLink.project.dto.resp;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
 
-import java.util.Date;
+import java.time.LocalDateTime;
 
 @Data
 public class ShortLinkPageRespDTO {
@@ -15,7 +15,9 @@ public class ShortLinkPageRespDTO {
     private String gid;
     private Integer validDateType;
     @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
-    private Date validDate;
+    private LocalDateTime validDate;
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
+    private LocalDateTime createdAt;
     private String description;
     private String favicon;
 }
